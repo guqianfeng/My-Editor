596dc215b2b05cce8369dabb69a19494cdfa66e1
qianfengg <fred_gqf0221@163.com>
2021-01-22 21:46:23 +0800

    init demo

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import './editor/Editor.ts'
+import './editor/editor.scss'
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+console.log('nb')
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          "style-loader",
+          // 将 CSS 转化成 CommonJS 模块
+          "css-loader",
+          // 将 Sass 编译成 CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '富文本案例',
+      template: path.resolve(__dirname, 'index.html')
+    }),
+  ],
+};
