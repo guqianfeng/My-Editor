393392ad943f8eb713cab3f2bf72e7ba1d25717f
qianfengg <fred_gqf0221@163.com>
2021-01-24 22:12:17 +0800

    子菜单的处理

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,5 +1,20 @@
 import Editor from "../editor/Editor";
 
+const subMenuDict = {
+  'head': [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+  ],
+  'color': [
+    'red',
+    'green',
+    'blue'
+  ]
+}
 export default class Menus {
   public editor: Editor
   constructor (editor: Editor) {
@@ -15,21 +30,39 @@ export default class Menus {
   }
 
   addMenu (type: string): void{
-    console.log(type);
     const toolEl = document.createElement('div');
     toolEl.className = 'editor-menu';
+    toolEl.innerHTML = type;
     /**
-     * 以下操作处理子菜单
+     * 子菜单处理
      */
-    switch (type) {
-      case 'bold':
-        toolEl.innerHTML = type;
-        break;
-      case 'head':
-        break;
-      case 'color':
-        break;    
+    if(['head', 'color'].includes(type)) {
+      /**
+       * head 有子菜单 h1-h6
+       * color 有子菜单，可以选择颜色
+       */
+      const subMenu = document.createElement('div');
+      subMenu.className = 'editor-sub-menu';
+      console.log(subMenuDict)
+      toolEl.appendChild(subMenu);
     }
     this.editor.toolbarEl.appendChild(toolEl);
+    this.addEvent(toolEl, type);
+  }
+
+  addEvent (el: HTMLElement, type: string): void {
+    if(['head', 'color'].includes(type)) {
+      const subMenuEl = el.querySelector('.editor-sub-menu') as HTMLElement;
+      el.addEventListener('mouseover', () => {
+        subMenuEl.style.display = 'block';
+      })
+      el.addEventListener('mouseout', () => {
+        subMenuEl.style.display = 'none';
+      })
+    } else {
+      el.addEventListener('click', () => {
+        console.log('加粗效果')
+      })
+    }
   }
 }
